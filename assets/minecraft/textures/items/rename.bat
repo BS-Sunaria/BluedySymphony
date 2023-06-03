47dbba203a533287e66ded938f28b8e1b1be0024
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-06-03 03:31:25 +0200

    #12 Modification de l'emplacement des fragments métiers + orbe et suppression des pommes dorées modifiées

--- assets/minecraft/textures/items/rename.bat
+++ assets/minecraft/textures/items/rename.bat
@@ -1,8 +1,8 @@
 setlocal enabledelayedexpansion
-for %%a in (*netherite*) do (
+for %%a in (*echo_upgraded_netherite*) do (
     set "filename=%%~na"
     set "extension=%%~xa"
-    set "newname=!filename:netherite=diamond!!extension!"
+    set "newname=!filename:echo_upgraded_netherite=diamond!!extension!"
     set "newname=!newname:~0!" 
     ren "%%a" "!newname!"
 )
