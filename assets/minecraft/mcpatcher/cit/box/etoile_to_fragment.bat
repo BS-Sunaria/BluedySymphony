cc04f76d24acd23ab2a1cc6e84b0490e54b3562c
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-06-03 03:32:29 +0200

    #11 Texture des étoiles métiers + Suppression .bat

--- assets/minecraft/mcpatcher/cit/box/etoile_to_fragment.bat
+++
@@ -1,8 +0,0 @@
-setlocal enabledelayedexpansion
-for %%a in (*etoile*) do (
-    set "filename=%%~na"
-    set "extension=%%~xa"
-    set "newname=!filename:etoile=fragment!!extension!"
-    set "newname=!newname:~0!" 
-    ren "%%a" "!newname!"
-)
